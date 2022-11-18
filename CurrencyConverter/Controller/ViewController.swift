9445c7b34e296266a682197579f57d28a957a871
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-18 15:58:06 +0300

    UserInterface oluşturuldu.

---
+++ CurrencyConverter/Controller/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Büşra Özkan on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+    @IBAction func getRatesButton(_ sender: Any) {
+        
+    }
+}
+
