1f4562b42dee5a6a8d4fdca2ad417c4e4668a2a0
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-18 19:42:37 +0300

    data işlenerek ilgili labellara aktarıldı.

--- CurrencyConverter/Controller/ViewController.swift
+++ CurrencyConverter/Controller/ViewController.swift
@@ -36,17 +36,38 @@ class ViewController: UIViewController {
         let task = session.dataTask(with: url!) { (data, response, error) in
             if error != nil {
                 let alert = UIAlertController(title: "Error!", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
-                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default,handler: nil)
                 alert.addAction(okButton)
-                self.present(alert, animated: true)
+                self.present(alert, animated: true,completion: nil)
             }else{
                 //(2. adım)
                 if data != nil {
                     do{
-                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
                         
                         DispatchQueue.main.async {
-                            print(jsonResponse)
+                            if let rates = jsonResponse["rates"] as? [String: Any]{
+                                
+                                if let cad = rates["CAD"] as? Double {
+                                    self.cadLabel.text = "CAD:\(cad)"
+                                }
+                                if let chf = rates["CHF"] as? Double {
+                                    self.chfLabel.text = "CHF:\(chf)"
+                                }
+                                if let gbp = rates["GBP"] as? Double {
+                                    self.gbpLabel.text = "GBP:\(gbp)"
+                                }
+                                if let jpy = rates["JPY"] as? Double {
+                                    self.jpyLabel.text = "JPY:\(jpy)"
+                                }
+                                if let usd = rates["USD"] as? Double {
+                                    self.usdLabel.text = "USD:\(usd)"
+                                }
+                                if let tr = rates["TRY"] as? Double {
+                                    self.tryLabel.text = "TRY:\(tr)"
+                                }
+                            }
+                            
                         }
                     }catch{
                     }
