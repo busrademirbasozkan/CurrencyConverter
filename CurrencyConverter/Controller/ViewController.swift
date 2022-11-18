98bb0425fc1a593dbc3ed2561870de835c57090c
busrademirbasozkan <busrademirbasozkan@gmail.com>
2022-11-18 16:30:39 +0300

    Url oluşturuldu.

--- CurrencyConverter/Controller/ViewController.swift
+++ CurrencyConverter/Controller/ViewController.swift
@@ -26,6 +26,27 @@ class ViewController: UIViewController {
 
 
     @IBAction func getRatesButton(_ sender: Any) {
+        // 1)Request & Session -> internetten veriye gitmek, istek göndermek
+        // 2)Response & Data -> Bu isteğe cevap almak, datayı almak
+        // 3)Parsing & JSON Serialization -> Bu datayı işlemek
+        
+        // URL tanımlamak (1. adım)
+        let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/CurrencyData/main/currency.json")
+        let session = URLSession.shared  //istediğiniz ağa gidip ordan veri alışverişi yapmak için kullanılır.
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error!", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
+            }else{
+                //(2. adım)
+                
+                
+                
+                
+            }
+        }
         
     }
 }
